394603c58bce6c0135049c1a0d577870e2541a62
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-09-23 15:09:10 +0200

    Read collection Student ID

---
+++ BlogWeb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlogWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Vejledningebooking01.ConsoleApp/Program.cs
+++ Vejledningebooking01.ConsoleApp/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Vejledningebooking01.Domain.DAL;
+using Vejledningebooking01.Domain.Model;
 
 namespace Vejledningebooking01.ConsoleApp
 {
@@ -6,7 +13,22 @@ namespace Vejledningebooking01.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var Context1 = new BookingContext(); // New DbContext   
+            var calendar2 = new Vejledningebooking01.Domain.Model.Calendar { Id = 1}; // Create new Calendar
+              
+            Context1.Calendars.Add(calendar2); // Add Calendar to Db
+            Context1.SaveChanges(); // Save
+
+            var calendars = Context1.Calendars.AsNoTracking().ToList(); // Get all Calendars
+
+
+            foreach (var c in calendars)   // Read Calendar
+            {
+                Console.WriteLine("IDDDDDDDDDDDD" + c.Id);
+            }
+
+            Console.ReadLine();
         }
     }
 }
